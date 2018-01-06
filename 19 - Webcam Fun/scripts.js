ea61dfcbd5f90fd66d0c39bf47a30dbd9c274975
Victor Li <victor.li158@gmail.com>
2018-01-06 14:22:53 -0800

    19 complete Webcam Fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,8 +26,9 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
     // pixels = redEffect(pixels);
-    pixels = rgbSplit(pixels);
-    ctx.globalAlpha = 0.8;
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+    pixels = greenScreen(pixels);
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
@@ -59,6 +60,33 @@ function rgbSplit(pixels) {
     pixels.data[i + 500] = pixels.data[i + 1];
     pixels.data[i - 550] = pixels.data[i + 2];
   }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (let i = 0; i < pixels.data.length; i = i + 4) {
+    const red = pixels.data[i + 0];
+    const green = pixels.data[i + 1];
+    const blue = pixels.data[i + 2];
+    const alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
   return pixels;
 }
 
